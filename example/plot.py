5c2517eb3b45430e402a448333bde237b1cc7419
Anand <anand@igi.tugraz.at>
2017-05-31 11:09:23 +0200

    Replacing Foobar with RealtimePlotter

--- example/plot.py
+++ example/plot.py
@@ -7,13 +7,13 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Foobar is distributed in the hope that it will be useful,
+# RealtimePlotter is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+# along with RealtimePlotter.  If not, see <http://www.gnu.org/licenses/>.
 #
 # For more information see: https://github.com/anandtrex/RealtimePlotter
 
--- example/simulation.py
+++ example/simulation.py
@@ -13,7 +13,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Realtimeplotter.  If not, see <http://www.gnu.org/licenses/>.
+# along with RealtimePlotter.  If not, see <http://www.gnu.org/licenses/>.
 #
 # For more information see: https://github.com/anandtrex/RealtimePlotter
 
